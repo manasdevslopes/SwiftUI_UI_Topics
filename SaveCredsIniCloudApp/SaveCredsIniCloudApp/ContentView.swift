78f36235ded223e1a07f219b92ad4f9dd9e2dba5
Manas Vijaywargiya <manasdevslopes@gmail.com>
2026-03-22 16:08:32 +0530

    Created SaveCredsIniCloudApp

--- SaveCredsIniCloudApp/SaveCredsIniCloudApp/ContentView.swift
+++ SaveCredsIniCloudApp/SaveCredsIniCloudApp/ContentView.swift
@@ -9,20 +9,50 @@
 //
     
 
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var username: String = ""
+  @State private var password: String = ""
+  
+  var body: some View {
+    VStack {
+      TextField("Username", text: $username)
+        .keyboardType(.emailAddress)
+      SecureField("Password", text: $password)
+        .keyboardType(.default)
+      Button("Log In") {
+        saveToSharedKeychain()
+      }
     }
+    .padding()
+  }
+  
+  private func saveToSharedKeychain() {
+    let domain = apiDomain
+    let user = username
+    let pass = password
+    
+    let cfDomain = domain as CFString
+    let cfUser = user as CFString
+    let cfPass = pass as CFString
+    
+    SecAddSharedWebCredential(cfDomain, cfUser, cfPass) { error in
+      DispatchQueue.main.async {
+        if let error {
+          print("Keychain Error:", error)
+        } else {
+          print("Credentials saved successfully")
+        }
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
+
+// When the domain hosted and the same need to be added here in Associated Domain in Capabilities - webcredentials:apidomain.com
+var apiDomain: String = "apidomain.com"
