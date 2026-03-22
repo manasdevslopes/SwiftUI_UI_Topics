cb13b05532d81ca297ea34c05f6b2b62e51a3526
Manas Vijaywargiya <manasdevslopes@gmail.com>
2026-03-22 16:05:16 +0530

    just fixed error here.

---
+++ SaveCredsIniCloudApp/SaveCredsIniCloudApp/SaveCredsIniCloudAppApp.swift
@@ -0,0 +1,21 @@
+//
+// SaveCredsIniCloudAppApp.swift
+// SaveCredsIniCloudApp
+//
+// Created by MANAS VIJAYWARGIYA on 22/03/26.
+// ------------------------------------------------------------------------
+// 
+// ------------------------------------------------------------------------
+//
+    
+
+import SwiftUI
+
+@main
+struct SaveCredsIniCloudAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
